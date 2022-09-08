2bb26be2313897789e9e91ba1475055fcfb7dab1
seongjo-seo <tjtjdwh5321@gmail.com>
2022-09-08 15:19:17 +0900

    안되는 개발 세팅 재설정

--- web/src/component/Header.jsx
+++ web/src/component/Header.jsx
@@ -3,10 +3,6 @@ import React from "react";
 const Header = () =>{
 	return(
 		<header>
-			<span className="Header_Logo">Logo</span>
-			<nav>
-				<a href="/"> 회사 소개</a>
-			</nav>
 		</header>
 	)
 }
--- web/src/pages/main.jsx
+++ web/src/pages/main.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import Header from "../component/Header";
-import './Main.css';
 
 const Main = () => {
 	return(
