28e1fa099c417a46b24a7ba462a2c80817926a8f
seongjo-seo <tjtjdwh5321@gmail.com>
2022-09-08 23:57:04 +0900

    style 예제 테스트

--- mobile/App.js
+++ mobile/App.js
@@ -1,12 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
 /** root component */
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>안녕하세요!</Text>
-      <StatusBar style="auto" />
+      <View>
+        <Text style={{margin: 16}} >안녕하세요! 다님을 이용해 주셔서 감사드려요!</Text>
+      </View>
+      <Button title="로그인"/>
+      <Button title="회원가입"/>
     </View>
   );
 }
